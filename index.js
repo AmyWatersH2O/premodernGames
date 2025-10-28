21d8a3146c57948b8e1feff33158e107a6f20233
Amy Waters <98133461+AmyWatersH2O@users.noreply.github.com>
2025-10-28 19:32:58 +0000

    added radians to degrees and degrees to radians

--- index.js
+++ index.js
@@ -6,8 +6,17 @@
             var diffY = Math.abs(pointA.y - pointB.y);
             return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
             
-        }
-    },
+        },
+        degreesToRadians(degrees){
+          return degrees * Math.PI / 180;
+        },
+        radiansToDegrees(radians)
+{
+  return radians * 180 / Math.PI;
+} 
+
+},
+
     phyz: {},
   };
 }(window, window._));

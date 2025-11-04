2320130e34e3f2e9ff00ba45821ae9a3fec9364d
Amy Waters <98133461+AmyWatersH2O@users.noreply.github.com>
2025-11-04 20:07:43 +0000

    added getAngleDegrees

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-(function(window, _) {
+(function (window, _) {
   window.premodernGames = window.premodernGames || {
     numz: {
-        calculateDistance(pointA, pointB){
-            var diffX = Math.abs(pointA.x - pointB.x);
-            var diffY = Math.abs(pointA.y - pointB.y);
-            return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
-            
-        },
-        degreesToRadians(degrees){
-          return degrees * Math.PI / 180;
-        },
-        radiansToDegrees(radians)
-{
-  return radians * 180 / Math.PI;
-} 
+      calculateDistance(pointA, pointB) {
+        var diffX = Math.abs(pointA.x - pointB.x);
+        var diffY = Math.abs(pointA.y - pointB.y);
+        return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
 
-},
+      },
+      degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+      },
+      radiansToDegrees(radians) {
+        return radians * 180 / Math.PI;
+      },
+
+  getAngleDegrees(pointA, pointB) {
+        const distanceX = pointB.x - pointA.x;
+        const distanceY = pointB.y - pointA.y;
+        const radians = Math.atan2(distanceY, distanceX);
+        const degrees = radians * 180 / Math.PI;
+        return degrees;
+      },
+
+    },
 
     phyz: {
       /**
@@ -40,83 +47,83 @@
  * force of impact of a collision.
  * @return {Object} The body.
  */
-makeBody: function(type, {
-  velocityX = 0,
-  velocityY = 0,
-  rotationalVelocity = 0,
-  integrity = 1,
-  density = 1,
-  volatility = 0
-} = {}) {
-  if (type === undefined) throw new Error('You must provide a valid String for the type parameter!');
-  return {
-    type: type,
-    velocityX: velocityX,
-    velocityY: velocityY,
-    rotationalVelocity: rotationalVelocity,
-    integrity: integrity,
-    density: density,
-    volatility: volatility,
+      makeBody: function (type, {
+        velocityX = 0,
+        velocityY = 0,
+        rotationalVelocity = 0,
+        integrity = 1,
+        density = 1,
+        volatility = 0
+      } = {}) {
+        if (type === undefined) throw new Error('You must provide a valid String for the type parameter!');
+        return {
+          type: type,
+          velocityX: velocityX,
+          velocityY: velocityY,
+          rotationalVelocity: rotationalVelocity,
+          integrity: integrity,
+          density: density,
+          volatility: volatility,
 
-    /**
-     * @param {Number} A number representing the force of the impact.
-     * @param {Object} The other body involved in the collision.
-     */
-    handleCollision(impact, body) {
-      // template method //
+          /**
+           * @param {Number} A number representing the force of the impact.
+           * @param {Object} The other body involved in the collision.
+           */
+          handleCollision(impact, body) {
+            // template method //
+          },
+
+          /**
+           * Can be overridden in the concrete body to provide a custom update()
+           * method.
+           */
+          update(event) {
+            // template method //
+          }
+        };
+      },
+
+      /**
+       * Updates the diagonal velocity properties of a body,
+       * taking into account the body's current velocity 
+       * and applying any forces acting against the body
+       * as acceleration on both the x and y axis.
+       * 
+       * NOTE: This method DOES NOT update the position of 
+       * the body, it only updates its velocity.
+       * 
+       * @param {Object} body: The body must be an Object 
+       * with velocityX, velocityY and rotation properties. 
+       * @param {Number} forceOnX: The force acting against
+       * the body on the x axis.
+       * @param {Number} forceOnY: The force acting against
+       * the body on the y axis.
+       */
+      updateVelocity(body, forceOnX, forceOnY) {
+        const
+          angle = body.rotation * Math.PI / 180,
+          accelerationOnX = Math.cos(angle) * forceOnX,
+          accelerationOnY = Math.sin(angle) * forceOnY;
+        body.velocityX += accelerationOnX;
+        body.velocityY += accelerationOnY;
+      },
+
+      /**
+       * Updates the x and y properties of a body based on its
+       * velocityX and velocityY, and, updates the rotation of
+       * a body based on its rotationalVelocity.
+       *
+       * @param {Object} body: The body must be an Object 
+       * with x, y, rotation, velocityX, velocityY, and 
+       * rotationalVelocity properties.
+       */
+      updatePosition(body) {
+        body.x += body.velocityX;
+        body.y += body.velocityY;
+        body.rotation += body.rotationalVelocity;
+      },
+
     },
 
-    /**
-     * Can be overridden in the concrete body to provide a custom update()
-     * method.
-     */
-    update(event) {
-      // template method //
-    }
-  };
-},
-
-/**
- * Updates the diagonal velocity properties of a body,
- * taking into account the body's current velocity 
- * and applying any forces acting against the body
- * as acceleration on both the x and y axis.
- * 
- * NOTE: This method DOES NOT update the position of 
- * the body, it only updates its velocity.
- * 
- * @param {Object} body: The body must be an Object 
- * with velocityX, velocityY and rotation properties. 
- * @param {Number} forceOnX: The force acting against
- * the body on the x axis.
- * @param {Number} forceOnY: The force acting against
- * the body on the y axis.
- */
-updateVelocity(body, forceOnX, forceOnY) {
-  const
-    angle = body.rotation * Math.PI / 180,
-    accelerationOnX  = Math.cos(angle) * forceOnX,
-    accelerationOnY = Math.sin(angle) * forceOnY;
-  body.velocityX += accelerationOnX;
-  body.velocityY += accelerationOnY;
-},
-
-/**
- * Updates the x and y properties of a body based on its
- * velocityX and velocityY, and, updates the rotation of
- * a body based on its rotationalVelocity.
- *
- * @param {Object} body: The body must be an Object 
- * with x, y, rotation, velocityX, velocityY, and 
- * rotationalVelocity properties.
- */
-updatePosition(body) {
-  body.x += body.velocityX;
-  body.y += body.velocityY;
-  body.rotation += body.rotationalVelocity;
-},
-
-    },
-    
   };
 }(window, window._));
